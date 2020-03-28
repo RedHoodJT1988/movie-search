d2b15ab404718fbe2163643e16e056ee5a8fb6fd
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2020-03-28 17:00:57 -0500

    added a-favor-house-atlantic.txt to the repo

--- speedway-trials-api/src/main/java/com/galvanize/repositories/DriverRepository.java
+++
@@ -1,7 +0,0 @@
-package com.galvanize.repositories;
-
-import com.galvanize.entities.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DriverRepository extends JpaRepository<Driver, Long> {
-}
--- speedway-trials-api/src/main/java/com/galvanize/repositories/RaceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.galvanize.repositories;
-
-import com.galvanize.entities.Race;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RaceRepository extends JpaRepository<Race, Long> {
-}
